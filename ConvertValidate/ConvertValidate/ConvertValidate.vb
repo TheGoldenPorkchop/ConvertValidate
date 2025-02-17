b557453b0889dd7d2de520674ef3a5ade6518064
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-02-16 20:31:48 -0700

    Add project files.

---
+++ ConvertValidate/ConvertValidate/ConvertValidate.vb
@@ -0,0 +1,56 @@
+﻿'Angel Nava
+'Spring 2025
+'RCET2265
+'ConvertandValidate
+'link
+Option Strict On
+Option Explicit On
+
+Module ConvertValidate
+
+    Sub Main()
+
+        'for testing the function add this to your Sub Main()
+
+        Dim aValidNumber As Integer
+        Dim usernResponse As String
+
+        Do
+
+            Console.WriteLine($"Enter a number:")
+
+            usernResponse = Console.ReadLine()
+
+            If ConversionValid(usernResponse, aValidNumber) = True Then
+
+                Console.WriteLine($"{usernResponse} converted successfully to {aValidNumber}!")
+
+            Else
+
+                Console.WriteLine($"Oops, {usernResponse} does not seem to be a number")
+
+            End If
+
+        Loop
+
+    End Sub
+
+    'make this work
+
+    Function ConversionValid(convertThisString As String, ByRef toThisInteger As Integer) As Boolean
+
+        Dim status As Boolean
+
+        Try
+            toThisInteger = Convert.ToInt32(convertThisString)
+            Return True
+        Catch ex As Exception
+            toThisInteger = 0
+            Return False
+        End Try
+
+        Return status
+
+    End Function
+
+End Module
